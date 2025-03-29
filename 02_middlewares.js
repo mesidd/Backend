3a877f4a4f51eb39b3fc983757011e8bf3506be8
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-29 13:20:46 +0530

    Middlewares

---
+++ 02_middlewares.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express()
+
+import fs from 'fs'
+
+import users from './MOCK_DATA.json' with {type : 'json'}
+
+const PORT = 8000
+
+// app.get('/', (req,res)=> {
+//   res.end("Welcome")
+// })
+
+app.use((req,res,next)=> {
+  fs.appendFile('log.txt', `\n${Date.now()}`, (err, data)=> {
+  })
+  console.log("Hello from Middleware 1")
+  req.myName = "Message from 1"
+  next();
+})
+
+app.use((req,res,next)=> {
+  console.log(req.myName)
+  return res.end("Hey middle ware 2")
+})
+
+app.get('/api/users',(req, res)=> {
+  res.json(users);
+})
+
+app.listen(PORT,()=>{
+  console.log("Server running on ",PORT)
+})
